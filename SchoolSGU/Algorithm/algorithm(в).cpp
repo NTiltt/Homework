42fe61b0fcc7b6257d59ec7248ddfe464e07b77d
NTiltt <62175120+NTiltt@users.noreply.github.com>
2020-04-27 11:25:06 +0400

    Задание В(Алгоритмы)

---
+++ SchoolSGU/Algorithm/algorithm(в).cpp
@@ -0,0 +1,67 @@
+//
+// Created by Никита Ясеник on 26.04.2020.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <map>
+#include <set>
+#include <iterator>
+#include <list>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <cstddef>
+
+
+#define sz(a) ((int)a.size())
+#define all(a) a.begin(), a.end()
+#define fore(i, l, r) for(int i = int(l); i < int(r); i++)
+#define forn(i, n) fore(i, 0, n)
+#define pb push_back
+#define x first
+#define y second
+#define mp make_pair
+
+using namespace std;
+
+//ifstream in("/Users/nikita/Documents/Atom/input.txt");
+//ofstream out("/Users/nikita/Documents/Atom/output.txt");
+
+typedef long double ld;
+
+const int N = int(1e6) + 111;
+const ld EPS = 1e-7;
+
+double len(int x, int y) {
+    return sqrt(x*x + y*y);
+}
+bool cmp(pair<int,double>p1,pair<int,double>p2) {
+    return p1.y > p2.y;
+}
+
+int main () {
+    vector <pair<int,double>> v;
+    vector <pair<int,int>> q;
+    int n;
+    cin >> n;
+    forn(i,n) {
+        int a,b;
+        cin >> a >> b;
+        q.pb(mp(a,b));
+        v.pb(mp(i,len(a,b)));
+    }
+    sort(all(v),cmp);
+    double w = v[0].y;
+    auto ind = find_if(all(v),[w](pair<int,double> p) {
+        return p.y != w;
+    });
+    v.erase(ind,v.end());
+    forn(i,sz(v)) {
+        cout << q[v[i].x].x << " " << q[v[i].x].y << endl;
+    }
+}
