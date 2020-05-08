9c1fb36e8c8c4f89e2921b489a2574bf993d80c7
NTiltt <62175120+NTiltt@users.noreply.github.com>
2020-05-08 10:31:12 +0400

    Task 6

---
+++ Task6/task_6_1.cpp
@@ -0,0 +1,48 @@
+//
+// @author: Nikita 
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <map>
+#include <set>
+#include <iterator>
+#include <list>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <cstddef>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <numeric>
+
+
+#define sz(a) ((int)a.size())
+#define all(a) a.begin(), a.end()
+#define fore(i, l, r) for(int i = int(l); i < int(r); i++)
+#define forn(i, n) fore(i, 0, n)
+#define pb push_back
+#define x first
+#define y second
+#define mp make_pair
+#define in cin
+#define out cout
+
+using namespace std;
+
+//ifstream in("/Users/nikita/Documents/Atom/input.txt");
+//ofstream out("/Users/nikita/Documents/Atom/output.txt");
+
+typedef long double ld;
+
+const int N = int(1e6) + 111;
+const ld EPS = 1e-7;
+
+int main () {
+    
+}
